bf8ec15d33804e272164f4f62f1dd93587ee6522
AaronChoi <aaronschoilcw@gmail.com>
2021-10-01 13:54:21 -0700

    Component Functional

--- src/components/MyModal/MyModal.tsx
+++ src/components/MyModal/MyModal.tsx
@@ -1,35 +1,89 @@
-import { useState } from "react";
-import genClassName from "../../util/genClassName";
+import { FC, useState } from "react";
+import { ButtonType } from "../MyButton/MyButton";
+import myGenClassName from "./util/myGenClassName";
 
-export default function MyModal(props) {
-    const { children } = props;
+enum ModalTypes {
+  Primary = "primary",
+  Secondary = "secondary",
+  Danger = "danger",
+  Default = "default",
+}
 
-    const [ toggle, setToggle ] = useState(false);
+enum SizeTypes {
+  Small = "sm",
+  Large = "lg",
+}
 
-  const content = children ? children : <div>Hi</div>;
+interface Props {
+  children?: FC;
+  text?: string;
+  modalType?: ModalTypes;
+  buttonType?: ModalTypes;
+  fontColor?: ModalTypes;
+  fontSize?: SizeTypes;
+  className?: string;
+}
+
+interface PrimaryStyling {
+  color: string;
+  size: string;
+  classes: string;
+}
+
+type ClassNameGenerator = () => string;
+
+export default function MyModal({
+  children,
+  text = "Alert!",
+  modalType = ModalTypes.Default,
+  buttonType = ModalTypes.Default,
+  fontColor = ModalTypes.Default,
+  fontSize = SizeTypes.Small,
+  className,
+}: Props) {
+  const [toggle, setToggle] = useState(true);
+
+  const content = children ? children : <div>{text}</div>;
 
   const toggleHandler = () => {
-      setToggle(true);
-  }
+    setToggle(true);
+  };
+
+  const primaryStyles: PrimaryStyling = {
+    color: fontColor ? fontColor : "",
+    size: fontSize ? fontSize : "",
+    classes: className ? className : "",
+  };
+
+  const buttonClassNames: ClassNameGenerator = () => {
+    const type = buttonType ? buttonType : "";
+    const { color, size } = primaryStyles;
+    const newStr = myGenClassName("btn", type, color, size, "").join(" ");
+    return `btn ${newStr}`;
+  };
+
+  const containerClassNames: ClassNameGenerator = () => {
+    const type = modalType ? modalType : "";
+    const { color, size, classes } = primaryStyles;
+    return myGenClassName("mymodal", type, color, size, classes).join(" ")
+  };
 
   const buttons = (
     <div>
-      <button onClick={toggleHandler}>Confirm</button>
+      <button className={buttonClassNames()} onClick={toggleHandler}>
+        Confirm
+      </button>
     </div>
   );
 
   const output = (
     <div className="mymodal-backdrop">
-      <div className="mymodal-container">
+      <div className={`mymodal-container ${containerClassNames()}`}>
         {content}
         {buttons}
       </div>
     </div>
   );
 
-  return (
-      <>
-    {toggle&&output}
-    </>
-  );
+  return <>{toggle && output}</>;
 }
